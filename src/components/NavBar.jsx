44917dcbfe684c3c208c1c49a45e8e9c02501ed5
objetive218 <davidsantiagotc@gmail.com>
2025-02-06 06:35:25 -0500

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import Register from "./Register";
 import Login from "./Login";
 import '../App.css'
 const NavBar = () => {
-    const { user, closeForm, setCloseForm, token, setUser, setToken, getPosts } = useContext(LoginContext)
+    const { user, closeForm, setCloseForm, token, setUser, setToken } = useContext(LoginContext)
     const [loginForm, setLoginForm] = useState(false)
-    const [file, setFile] = useState({});
+
     async function handleLogout(e) {
         e.preventDefault();
         const res = await fetch("/api/logout", {
@@ -26,42 +26,12 @@ const NavBar = () => {
         }
     }
 
-    const handleFileChange = (event) => {
-        setFile(event.target.files[0]);
-    };
 
-    const handleUpload = async () => {
-        if (!file) {
-            alert("Selecciona un archivo primero");
-            return;
-        }
 
-        const formData = new FormData();
-        formData.append("file", file);
 
-        try {
-            const response = await fetch("/api/import-excel", {
-                method: "post",
-                body: formData,
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
 
-            const result = await response.json();
-            console.log(result);
-            alert("Archivo importado correctamente");
-            getPosts()
-        } catch (error) {
-            console.error("Error al subir el archivo:", error);
-            alert("Error al importar el archivo");
-        }
-    };
-    
     return (
         <nav>
-            <input type="file" accept=".xlsx,.xls" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Cargar</button>
             <button className="home">Home</button>
             {user ? "" : <button className="register" onClick={(e) => setCloseForm(!closeForm)}>Registrarse</button>}
             {closeForm && !user ? <Register /> : ""}
@@ -72,7 +42,7 @@ const NavBar = () => {
                 </form>
             </>
             ) : ""}
-            {user ?"": <button onClick={() => setLoginForm(!loginForm)} className="login">Login</button>}
+            {user ? "" : <button onClick={() => setLoginForm(!loginForm)} className="login">Login</button>}
             {loginForm ? <Login setLoginForm={setLoginForm} /> : ""}
         </nav>
     );
